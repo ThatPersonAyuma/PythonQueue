e21c769e9b595ef65896e924c95d06a9c4abb4b1
Ayuma <adityajatisari@gmail.com>
2025-09-17 20:33:40 +0700

    Finish LinkedList and QueueLS

--- DS.py
+++ DS.py
@@ -1,15 +1,24 @@
 import array
-from collections import defaultdict
 class LinkedList:
     def __init__(self, val:any=0, next:"LinkedList"=None): # Use string instead of data type cause Python didn't know it yet
         self.val = val
         self.next = next
-        
+    def travel(self, index: int):
+        if (index<0):
+            raise IndexError("Index Can't be A Negative Number")
+        current = self
+        while(index>0 and current.next!=None):
+            current = current.next
+            index-=1
+        if index!=0:
+            raise IndexError("Out of Range")
+        self.val = current.val  # Use this to do inplace but return it if don't
+        self.next = current.next
 # (Enqueue, Dequeue, Peek/Front, IsEmpty, IsFull)
 class QueueArr:
-    def __init__(self, initialNum: int|float=0, dataType: chr='i'):
-        self.arr = array.array(dataType, [0]*initialNum)
-        self.capacity = initialNum  # kapasitas max
+    def __init__(self, Max: int=0, dataType: chr='i'):
+        self.arr = array.array(dataType, [0]*Max)
+        self.capacity = Max  # kapasitas max
         self.size = 0               # banyak data
         self.front = 0              # indeks depan
         self.rear = -1              # indeks belakang
@@ -33,6 +42,38 @@ class QueueArr:
         return self.size==0
     def IsFull(self):
         return self.capacity==self.size
-# class QueueLS:
-#     def __init__(self, initialNum: int|float):
-#         self.
+class QueueLS:
+    def __init__(self, Max: int):
+        self.capacity = Max
+        self.size = 0
+        self.front:LinkedList=None
+        self.rear:LinkedList=None
+    def peek_front(self)->int|float:
+        if(self.front==None):
+            return self.front
+        return self.front.val
+    def peek_back(self)->int|float:
+        if(self.rear==None):
+            return self.rear
+        return self.rear.val
+    def Enqueue(self, val: int|float):
+        if self.IsFull():
+            raise IndexError("Queue Penuh")
+        if self.IsEmpty():
+            self.front=LinkedList(val=val)
+            self.rear=self.front
+        else:
+            self.rear.next=LinkedList(val=val)
+            self.rear=self.rear.next
+        self.size+=1
+    def Dequeue(self):
+        if self.IsEmpty():
+            raise IndexError("Queue Kosong")
+        self.front=self.front.next
+        if(self.front==None):
+            self.rear=None
+        self.size -=1
+    def IsEmpty(self):
+        return self.size==0
+    def IsFull(self):
+        return self.capacity==self.size
